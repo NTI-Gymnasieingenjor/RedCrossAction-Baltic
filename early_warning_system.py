6563bb7bef6b88b0f904ce507f99cc4e66aae443
Fredrik Nyberg <fredriknyberg1@gmail.com>
2020-10-14 10:54:14 +0200

    Name change
    Changed file names to fit Readme file
    Nake sure that our gmail_test works for 2 diffrent cases
    Removed twitter api files

--- early_warning_system.py
+++ early_warning_system.py
@@ -1,73 +1,101 @@
 # Imports mail modules
+from __future__ import print_function
 import smtplib, ssl
 import csv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-
-# For SSL
-port = 465
-
-# The mail you send from
-sender_email = ("python.ormar@gmail.com")
-
-# The password associated with the mail you typed in
-password = input("Skriv in ditt lösenord: ")
-
-text = """\
-Hej!
-Jag skickade detta meddelande från Python!
-Mvh
-PythonOrmarna
-"""
-
-html="""\
-<html>
-     <head>
-         <meta charset="utf-8">
-     </head>
-     <body>
-         <h1>Hej!</h1>
-         <h2>Jag skickade detta meddelande från Python!</h2>
-        
-         <p>Mvh<br>
-             <strong>PythonOrmarna</strong>
-         </p>
-     </body>
- </html>
-"""
+from datetime import datetime
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import time
+import pickle
+import os.path
+import sys
+import pytz
+import base64
 
 
-# The message you choose to send
-email_message = MIMEMultipart("alternative")
-email_message["Subject"] = "Hej på dig!"
-email_message["From"] = sender_email
 
-# Turn these into plain/html MIMEText objects
-part1 = MIMEText(text, "plain")
-part2 = MIMEText(html, "html")
+def create_message(sender, to, subject, message_text):
+    """Create a message for an email.
+      Args:
+          sender: Email address of the sender.
+          to: Email address of the receiver.
+          subject: The subject of the email message.
+          message_text: The text of the email message.
+      Returns:
+          An object containing a base64url encoded email object.
+    """
+    message = MIMEText(message_text)
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    return {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
 
-# Add HTML/plain-text parts to MIMEMultipart message
-# The email client will try to render the last part first
-email_message.attach(part1)
-email_message.attach(part2)
 
-# Create a secure SSL context
-context = ssl.create_default_context()
+def send_message(service, user_id, message):
+    """Send an email message.
+      Args:
+          service: Authorized Gmail API service instance.
+          user_id: User's email address. The special value "me"
+          can be used to indicate the authenticated user.
+          message: Message to be sent.
+      Returns:
+          Sent Message.
+    """
+    
+    message = (service.users().messages().send(userId=user_id, body=message).execute())
+    print('Message Id: %s' % message['id'])
+    return message
 
-# Sending message
-print("Skickar meddelande!")
 
-# Connect to smtp mail server
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    # Logs in to the mail server
-    server.login(sender_email, password)
-    with open("contacts.csv") as file:
-        reader = csv.reader(file)
-        next(reader)  # Skip header row
-        for email in reader:
-            server.sendmail(sender_email, email, email_message.as_string())
-            print(f"Sending email...")
-            # Send email here
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
-# Confirmation message
-print("Meddelande skickat!")
+def get_service(path):
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    scores = {} # scores is an empty dict already
+
+    if os.path.getsize('token.pickle') > 0:      
+        with open('token.pickle', "rb") as f:
+            unpickler = pickle.Unpickler(f)
+        # if file is not empty scores will be equal
+        # to the value unpickled
+            scores = unpickler.load()
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+    # Startar Gmail v1 med den som är inloggad
+    service = build('gmail', 'v1', credentials=creds)
+ 
+
+path_to_pickle = r"C:\Users\fredrik.nyberg\Documents\GitHub\RedCrossAction-Baltic\token.pickle"
+subject = "Hi! from python"
+sender = "python.ormar@gmail.com"
+to = "python.ormar@gmail.com"
+message_text = "This e-mail is sent from Gmail API via python! Isn't that Cool?"
+
+user_id = "me"
+
+service = get_service(path_to_pickle)
+raw_text = create_message(sender, to, subject, message_text)
+message_data = send_message(service, user_id, raw_text)
